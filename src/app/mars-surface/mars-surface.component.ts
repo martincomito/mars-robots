a1724350c5e439043fb710b22d0234d910708f1d
Martín Comito <martin.l.comito@accenture.com>
2019-06-04 21:12:10 -0300

    Created createSurface function to draw the grid from the input values

--- src/app/mars-surface/mars-surface.component.ts
+++ src/app/mars-surface/mars-surface.component.ts
@@ -8,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class MarsSurfaceComponent implements OnInit {
 
   constructor() { }
+  
+  column: number[];
+  row: number[];
 
   ngOnInit() {
+  }
+  
+  createSurface = (size) => {
+    let x = size.split(' ')[0];
+    let y = size.split(' ')[1];
+    this.row = [];
+    this.column = []; 
+    for (let i = 1; i <= y; i++) {
+      this.column.push(i)
+    } 
+    for (let i = 1; i <= x; i++) {
+      this.row.push(i)
+   } 
+   this.row = this.row.reverse();
   }
 
 }
